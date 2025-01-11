a82ef341481b597fc3090e2a13119c3355eed17d
Alejandro Merenciano <alemeco2005@gmail.com>
2025-01-11 18:52:17 +0100

    Commit arreglo

--- src/GridFilms.tsx
+++ src/GridFilms.tsx
@@ -21,21 +21,20 @@ export function GridFilms({ handleUpdate} : { handleUpdate:(id:number)=>void}) {
             }
           };
 
-          return (
-            <div className="container mx-auto p-4">
-                <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-                    {filmEntries.map(pelicula => (
-                        <Film 
-                            key={pelicula.id} 
-                            id={pelicula.id}
-                            image={pelicula.image} 
-                            name={pelicula.name} 
-                            year={pelicula.year}
-                            handleEliminar={handleEliminar}
-                            handleUpdate={handleUpdate}
-                        />
-                    ))}
-                </ul>
-            </div>
-        );
+    return (
+
+        <ul className="flex flex-row justify-center">
+            {filmEntries.map(pelicula => (
+                <Film 
+                key={pelicula.id} 
+                id={pelicula.id}
+                image={pelicula.image} 
+                name={pelicula.name} 
+                year={pelicula.year}
+                handleEliminar={handleEliminar}
+                handleUpdate={handleUpdate}
+                />
+            ))}
+        </ul>
+    )
 }
